90ec4cf96a4e8eb0e8e51a8ce84d93051bdd3914
rmxtronic <rmxtronic@gmail.com>
2024-01-10 03:38:03 -0500

    creation and compile

--- src/main/java/com/sms/SmsApplication.java
+++ src/main/java/com/sms/SmsApplication.java
@@ -2,8 +2,11 @@ package com.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+//Had to add the (exclude = {DataSourceAutoConfiguration.class }) to avoid asking for database URL
+
 public class SmsApplication {
 
 	public static void main(String[] args) {
