ba4dda43f94e4e667d4ebb9ba96dcee6cd6acb19
rmxtronic <rmxtronic@gmail.com>
2024-01-16 05:19:54 -0500

    html and database compiling fine (pending to test functionality)

--- src/main/java/com/sms/SubscriptionController.java
+++ src/main/java/com/sms/SubscriptionController.java
@@ -1,15 +1,14 @@
 package com.sms;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 
-@RequestMapping("/api/subscriptions")
+//@RequestMapping("/api/subscriptions")
 
-public
-
-class SubscriptionController {
+public class SubscriptionController {
 
     @Autowired
     private SubscriptionPlanManager subscriptionPlanManager;
